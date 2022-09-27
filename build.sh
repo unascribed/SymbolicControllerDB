ece9deb3344310359ab17cf9d763060c532c6fba
Una Thompson <me@unascribed.com>
2022-09-27 00:39:18 -0700

    Fixes

--- build.sh
+++ build.sh
@@ -10,14 +10,14 @@ mkdir -p dist
 			continue
 		fi
 
-		jq -M '.name as $name|[.bg as $defBg|.fg as $defFg|.buttons|keys[] as $k|.[$k] as {$type,$bg,$fg} ?// $type|{key:($k),value:{type:($type),bg:($bg // $defBg),fg:($fg // $defFg)}}]|from_entries|{name:$name,buttons:.}' "$j"
 		if [ "$first" == "1" ]; then
 			first=0
 		else
 			echo ','
 		fi
+		jq -M '.name as $name|[.bg as $defBg|.fg as $defFg|.buttons|keys[] as $k|.[$k] as {$type,$bg,$fg} ?// $type|{key:($k),value:{type:($type),bg:($bg // $defBg),fg:($fg // $defFg)}}]|from_entries|{name:$name,buttons:.}' "$j"
 	done
 	echo '],"symbols":'
 	cat symbols.json
 	echo '}'
-) | jq -acM 'del(..|.["_comment"]?)' > dist/symboliccontrollerdb.json
+) |tee /dev/stderr | jq -acM 'del(..|.["_comment"]?)' > dist/symboliccontrollerdb.json
